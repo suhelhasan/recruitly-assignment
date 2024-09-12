fa40d8036054e6b84fada32627652362c7bdf6f5
Suhel Hasan <suhelhasan202@gmail.com>
2024-09-12 19:10:29 +0530

    final bug fix

--- src/pages/CompanyListPage.jsx
+++ src/pages/CompanyListPage.jsx
@@ -1,18 +1,18 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import Companies from "../components/Companies";
 import Header from "../components/Header";
-// import { useAuth } from "../context/AuthContext";
-// import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const CompanyListPage = () => {
-  // const { isAuthenticated } = useAuth();
-  // const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     navigate("/");
-  //   }
-  // }, [isAuthenticated]);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated]);
 
   return (
     <div>
